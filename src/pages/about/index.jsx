d5ad2abd9ab7942066f4f54efbc6f4ae02d217d2
Bishal Joshi <bishal.joshi@maitriservices.com>
2024-07-24 11:06:47 +0545

    unit test

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function About() {
   return (
     <div>
-      <h1>About</h1>
+      <h1>About </h1>
       <Link href="/about">About</Link>
     </div>
   );

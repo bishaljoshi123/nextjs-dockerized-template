544810d8f6fbd0333a2e13e080d29143a2a92925
Bishal Joshi <bishal.joshi@maitriservices.com>
2024-07-24 12:51:22 +0545

    test

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Link from "next/link";
+// import Link from "next/link";
 
 export default function About() {
   return (
     <div>
-      <h1>About</h1>
-      <Link href="/about">About</Link>
+      {/* <h1>About</h1> */}
+      {/* <Link href="/about">About</Link> */}
     </div>
   );
 }

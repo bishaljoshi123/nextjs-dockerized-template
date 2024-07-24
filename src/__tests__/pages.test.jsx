5c349a7012a61c5226d0808d505f78034c7b5883
Bishal Joshi <bishal.joshi@maitriservices.com>
2024-07-24 10:41:25 +0545

    unit test

---
+++ src/__tests__/pages.test.jsx
@@ -0,0 +1,10 @@
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import About from "../pages/about/index";
+
+test("About", () => {
+  render(<About />);
+  expect(
+    screen.getByRole("heading", { level: 1, name: "About" })
+  ).toBeDefined();
+});

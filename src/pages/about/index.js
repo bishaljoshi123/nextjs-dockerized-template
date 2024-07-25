da68b39f5a574c25b0ec3508ecefb826a082b763
Bishal Joshi <bishal.joshi@maitriservices.com>
2024-07-25 10:20:21 +0545

    jest test added

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function About() {
   return (
     <div>
-      {/* <h1>About</h1>
-      <Link href="/about">About</Link> */}
+      <h1>About</h1>
+      <Link href="/about">About</Link>
     </div>
   );
 }
